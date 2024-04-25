2d9283799b127be25be12d152f55134aa16893d6
bogdan9876 <bogdankryveiko@gmail.com>
2024-04-25 16:21:50 +0300

    support intermediate result

--- src/components/Main/supportAction/support.js
+++ src/components/Main/supportAction/support.js
@@ -1,16 +1,13 @@
 import styles from './support.module.css';
 
-
 const SupportAction = () => {
   return (
     <div className={styles.supportActionContainer}>
       <div className={styles.contentWrapper}>
-        <img src="./main-page/blaze.svg" alt="Blaze" className={styles.blazeImage} />
-        <p className={styles.supportActionText}>Хочеш долучитись до збору коштів або маєш нагальну потребу, яку потрібно закрити?</p>
-        <div className={styles.secondaryContent}>
-          <p className={styles.secondaryText}>Реєструйся та долучайся</p>
-          <img src="./main-page/double-arrow.svg" alt="Double Arrow" className={styles.doubleArrowImage} />
-        </div>
+        <span className={styles.wrapper}>
+          <img src="./main-page/blaze.svg" alt="Blaze" className={styles.blazeImage} />
+          <p className={styles.supportActionText}>Хочеш долучитись до збору коштів або маєш нагальну потребу, яку потрібно закрити?</p>
+        </span>
       </div>
     </div>
   );
