8976e8d7bc8a505e1fc8ad2bc550d4474c0067f0
bogdan9876 <bogdankryveiko@gmail.com>
2024-04-25 17:13:57 +0300

    fix footer and add adaptive resolution

--- src/app/page.js
+++ src/app/page.js
@@ -2,14 +2,17 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import Footer from '../components/Footer/Footer';
 // import Section1 from '../components/Main/section1';
-import HelpUkraine from '../components/Main/helpUkraine/helpUkraine';
-import SupportAction from '../components/Main/SupportAction/support';
+// import HelpUkraine from '../components/Main/helpUkraine/helpUkraine';
+// import SupportAction from '../components/Main/SupportAction/support';
+import Header from '../components/Header/Header';
 
 export default function Home() {
   return (
     <>
-    <SupportAction />
-    <HelpUkraine />
-    <Footer /></>
+    {/* <SupportAction />
+    <HelpUkraine /> */}
+    {/* <Header /> */}
+    <Footer />
+    </>
   );
 }
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,41 +2,45 @@ import styles from './footer.module.css';
 
 const Footer = () => {
   return (
-    <div className={styles.footer}>
-      <div className={styles.footerinfo}>
-        <div className={styles.footeremail}>
-          <div className={styles.footeremail1}>
-            <p>Надішли свій Email та отримуй<br />сповіщення про нові запити</p>
-          </div>
-          <div className={styles.footeremail2}>
-            <input type="email" placeholder="твій Email..." />
-          </div>
-        </div>
-        <div className={styles.socialnet}>
-          <div className={styles.logo}>
-            <img src="/logo.svg" alt="Логотип" />
-            <div className={styles.logoText}>
-              Допомога в дії
-            </div>
-          </div>
-          <div className={styles.aboutus}>
-            <p>Про нас</p>
-            <p>Зареєструватись</p>
-            <p>Потреби</p>
-            <p>FAQ</p>
-          </div>
-          <div className={styles.icons}>
-            <img src="/instagram.svg" alt="Instagram" />
-            <img src="/facebook.svg" alt="Instagram" />
-            <img src="/x.svg" alt="Instagram" />
-            <img src="/linkedin.svg" alt="Instagram" />
-          </div>
-        </div>
+<div className={styles.footer}>
+ 
+  <div className={styles.footerinfo}>
+    <div className={styles.footeremail}>
+      <div className={styles.footeremail1}>
+        <p>
+          Надішли свій Email та отримуй<br />
+          сповіщення про нові запити
+        </p>
       </div>
-      <div className={styles.content}>
-        <p>Всі права захищено</p>
+      <div className={styles.footeremail2}>
+        <input type="email" placeholder="твій Email..." />
       </div>
     </div>
+    <div className={styles.socialnet}>
+      <div className={styles.leftSection}>
+        <div className={styles.logo}>
+          <img src="/logo.svg" alt="Логотип" />
+          <div className={styles.logoText}> Допомога в дії </div>
+        </div>
+        <div className={styles.aboutus}>
+          <p>Про нас</p>
+          <p>Зареєструватись</p>
+          <p>Потреби</p>
+          <p>FAQ</p>
+        </div>
+      </div>
+      <div className={styles.icons}>
+        <img src="/instagram.svg" alt="Instagram" />
+        <img src="/facebook.svg" alt="Instagram" />
+        <img src="/x.svg" alt="Instagram" />
+        <img src="/linkedin.svg" alt="Instagram" />
+      </div>
+    </div>
+  </div>
+  <div className={styles.content}>
+    <p>Всі права захищено</p>
+  </div>
+</div>
   );
 };
 
