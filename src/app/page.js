7fa5e16860541abd8ecdb2d194c5f50eee0817a8
bogdan9876 <bogdankryveiko@gmail.com>
2024-04-25 14:56:38 +0300

    add reb banner on main

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Footer from '../components/Footer/Footer';
-
+// import Section1 from '../components/Main/section1';
+import HelpUkraine from '../components/Main/helpUkraine';
 
 export default function Home() {
   return (
-    <><p>Hello</p><Footer /></>
+    <>
+    <HelpUkraine />
+    <Footer /></>
   );
 }
