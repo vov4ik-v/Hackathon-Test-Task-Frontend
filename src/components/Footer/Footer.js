1c75c2abee57af4afc5c4be34ca48949e099d780
bogdan9876 <bogdankryveiko@gmail.com>
2024-04-25 11:50:43 +0300

    footer intermediate result 2

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,12 +5,22 @@ const Footer = () => {
     <footer className={styles.footer}>
       <div className={styles.footerinfo}>
         <div className={styles.footeremail}>
-        <div className={styles.footeremail1}>
-        <p>Надішли свій Email та отримуй<br />сповіщення про нові запити</p>
+          <div className={styles.footeremail1}>
+            <p>Надішли свій Email та отримуй<br />сповіщення про нові запити</p>
+          </div>
+          <div className={styles.footeremail2}>
+            <input type="email" placeholder="твій Email..." />
+          </div>
         </div>
-        <div className={styles.footeremail2}>
-        <input type="email" placeholder="твій Email..." />
-        </div>
+        <div className={styles.socialnet}>
+          <div className={styles.logo}>
+            <img src="/logo.svg" alt="Логотип" />
+            <div className={styles.logoText}>
+              Допомога в дії
+            </div>
+          </div>
+          <div className={styles.aboutus}></div>
+          <div className={styles.icons}></div>
         </div>
       </div>
       <div className={styles.content}>
