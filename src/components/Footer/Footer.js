342f2b15c49c65da0cb0936e25a9cd9f085f5af3
bogdan9876 <bogdankryveiko@gmail.com>
2024-04-25 11:37:29 +0300

    footer intermediate result

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,15 +1,23 @@
 import styles from './footer.module.css';
 
 const Footer = () => {
-    return (
-      <footer className={styles.footer}>
-        <div className={styles.footerinfo}></div>
-        <div className={styles.content}>
-            <p>Всі права захищено</p>
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.footerinfo}>
+        <div className={styles.footeremail}>
+        <div className={styles.footeremail1}>
+        <p>Надішли свій Email та отримуй<br />сповіщення про нові запити</p>
         </div>
-      </footer>
-    );
-  };
-  
-  export default Footer;
-  
+        <div className={styles.footeremail2}>
+        <input type="email" placeholder="твій Email..." />
+        </div>
+        </div>
+      </div>
+      <div className={styles.content}>
+        <p>Всі права захищено</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
