4d7df87b3c1d0cb2edcca063b316299f525cecb6
bogdan9876 <bogdankryveiko@gmail.com>
2024-04-26 19:49:06 +0300

    almost done

--- src/components/Main/section1/section1.js
+++ src/components/Main/section1/section1.js
@@ -3,10 +3,28 @@ import styles from './section1.module.css';
 const Section1 = () => {
   return (
     <div className={styles.section1}>
-      <span className={styles.firstLine}>
-        Ми - це зосередження запитів про термінові потреби людей, військових, з метою полегшення пошуку інформації охочим допомогти
-      </span>
-      <img src="/main-page/section11.svg" alt="Section 1 Image" className={styles.image} />
+      <div className={styles.section11}>
+        <span className={styles.firstLine}>
+          Ми - це зосередження запитів про термінові потреби людей, військових,
+           з метою полегшення пошуку інформації охочим допомогти
+        </span>
+        <img src="/main-page/section11.svg" alt="Section 1 Image" className={styles.image} />
+      </div>
+      <div className={styles.bottomText}>
+        ДОПОМОГА
+      </div>
+      <div className={styles.section12}>
+        <div className={styles.imageWrapper}>
+          <img src="/main-page/section12.svg" alt="Section 12 Image" className={styles.image} />
+        </div>
+        <div className={styles.textBesideImage}>
+          <span style={{ color: 'black' }}>В</span> ДІЇ
+          <button className={styles.registerButton}>Зареєструватись</button>
+        </div>
+      <div className={styles.endedText}>
+        Хочеш долучитись до збору коштів або маєш нагальну потребу, яку потрібно закрити?
+      </div>
+      </div>
     </div>
   );
 };
