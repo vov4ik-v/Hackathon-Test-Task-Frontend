bd0a567cd203b15403f84934a84314e63b5642ae
bogdan9876 <bogdankryveiko@gmail.com>
2024-04-26 15:30:26 +0300

    fix hightlight blue color selector

--- src/app/needs/modal.js
+++ src/app/needs/modal.js
@@ -1,11 +1,44 @@
+import React, { useState } from 'react';
 import styles from './modal.module.css';
 
 const Modal = () => {
+    const [selectedOption, setSelectedOption] = useState('');
+    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+    const handleOptionClick = (option) => {
+        setSelectedOption(option);
+        setIsDropdownOpen(false);
+    };
+
     return (
         <div className={styles.modal}>
             <h2 className={styles.title}>Бажаєш зареєструвати потребу?</h2>
             <p className={styles.dataInput}>Введи дані:</p>
-            {/* Додайте сюди селектор для типу потреби та інші поля */}
+            <div className={styles.selectWrapper}>
+                <div
+                    className={styles.selectedOption}
+                    onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+                >
+                    {selectedOption || 'Виберіть опцію'}
+                    <img src="/arrow.svg" alt="Arrow" className={styles.arrowIcon} />
+                </div>
+                {isDropdownOpen && (
+                    <div className={styles.optionsContainer}>
+                        <div
+                            className={styles.option}
+                            onClick={() => handleOptionClick('Збір')}
+                        >
+                            Збір
+                        </div>
+                        <div
+                            className={styles.option}
+                            onClick={() => handleOptionClick('Гуманітарна допомога')}
+                        >
+                            Гуманітарна допомога
+                        </div>
+                    </div>
+                )}
+            </div>
         </div>
     );
 };
