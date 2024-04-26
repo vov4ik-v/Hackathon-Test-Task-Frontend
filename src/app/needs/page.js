3204c0bda1f3e3242a72fdefd05e8f6a620dedeb
bogdan9876 <bogdankryveiko@gmail.com>
2024-04-26 14:41:44 +0300

    first section of need page

---
+++ src/app/needs/page.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import styles from './needs.module.css';
+
+const Needs = () => {
+    return (
+        <>
+            <p className={styles.main}>
+                Головна / <span className={styles.needText}>Потреби</span>
+            </p>
+            <div className={styles.needsText}>
+                Потреби
+            </div>
+            <div className={styles.needsContainer}>
+                <div className={styles.leftSection}>
+                    <p className={styles.leftSectionText}>
+                        На цій сторінці ви знайдете інформацію про термінові потреби людей,
+                        військових та інших осіб, які потребують допомоги в різних сферах життя.
+                    </p>
+                    <button className={styles.registerButton}>Зареєструватись</button>
+                </div>
+                <div className={styles.rightSection}>
+                    <img src="/flag.svg" alt="Flag" className={styles.flagImage} />
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Needs;
