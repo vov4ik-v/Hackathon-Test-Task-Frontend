510b66c7e323d3f2390ee8d37372d73116aaa6b2
bogdan9876 <bogdankryveiko@gmail.com>
2024-04-26 16:04:21 +0300

    next changes modal window

--- src/app/needs/modal.js
+++ src/app/needs/modal.js
@@ -2,12 +2,22 @@ import React, { useState } from 'react';
 import styles from './modal.module.css';
 
 const Modal = () => {
-    const [selectedOption, setSelectedOption] = useState('');
-    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+    const [selectedOption1, setSelectedOption1] = useState('');
+    const [selectedOption2, setSelectedOption2] = useState('');
+    const [isDropdownOpen1, setIsDropdownOpen1] = useState(false);
+    const [isDropdownOpen2, setIsDropdownOpen2] = useState(false);
 
-    const handleOptionClick = (option) => {
-        setSelectedOption(option);
-        setIsDropdownOpen(false);
+    const handleOptionClick1 = (option) => {
+        setSelectedOption1(option);
+        setIsDropdownOpen1(false);
+        if (option !== 'Збір') {
+            setIsDropdownOpen2(false);
+        }
+    };
+
+    const handleOptionClick2 = (option) => {
+        setSelectedOption2(option);
+        setIsDropdownOpen2(false);
     };
 
     return (
@@ -17,28 +27,55 @@ const Modal = () => {
             <div className={styles.selectWrapper}>
                 <div
                     className={styles.selectedOption}
-                    onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+                    onClick={() => setIsDropdownOpen1(!isDropdownOpen1)}
                 >
-                    {selectedOption || 'Виберіть опцію'}
+                    {selectedOption1 || 'Виберіть опцію'}
                     <img src="/arrow.svg" alt="Arrow" className={styles.arrowIcon} />
                 </div>
-                {isDropdownOpen && (
+                {isDropdownOpen1 && (
                     <div className={styles.optionsContainer}>
                         <div
                             className={styles.option}
-                            onClick={() => handleOptionClick('Збір')}
+                            onClick={() => handleOptionClick1('Збір')}
                         >
                             Збір
                         </div>
                         <div
                             className={styles.option}
-                            onClick={() => handleOptionClick('Гуманітарна допомога')}
+                            onClick={() => handleOptionClick1('Гуманітарна допомога')}
                         >
                             Гуманітарна допомога
                         </div>
                     </div>
                 )}
             </div>
+            {selectedOption1 === 'Збір' && (
+                <div className={styles.selectWrapper}>
+                    <div
+                        className={styles.selectedOption}
+                        onClick={() => setIsDropdownOpen2(!isDropdownOpen2)}
+                    >
+                        {selectedOption2 || 'Виберіть опцію'}
+                        <img src="/arrow.svg" alt="Arrow" className={styles.arrowIcon} />
+                    </div>
+                    {isDropdownOpen2 && (
+                        <div className={styles.optionsContainer}>
+                            <div
+                                className={styles.option}
+                                onClick={() => handleOptionClick2('Option 1')}
+                            >
+                                Option 1
+                            </div>
+                            <div
+                                className={styles.option}
+                                onClick={() => handleOptionClick2('Option 2')}
+                            >
+                                Option 2
+                            </div>
+                        </div>
+                    )}
+                </div>
+            )}
         </div>
     );
 };
