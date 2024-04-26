1d0afd6b89ab8fc1877d2e9bffeecd464e627a3d
bogdan9876 <bogdankryveiko@gmail.com>
2024-04-26 23:43:34 +0300

    mini component add

--- src/app/needs/page.js
+++ src/app/needs/page.js
@@ -32,6 +32,40 @@ const Needs = () => {
                     <img src="/flag.svg" alt="Flag" className={styles.flagImage} />
                 </div>
             </div>
+            <div className={styles.extraHelpContainer}>
+            <h2 className={styles.title}>Нагальна потреба</h2>
+            <div className={styles.sectionsContainer}>
+                <div className={styles.leftSection1}>
+                    <div className={styles.item}>
+                        <span className={styles.itemText}>потрібна сума</span>
+                        <span className={styles.itemNumber}>100 000 +</span>
+                    </div>
+                    <div className={styles.item}>
+                        <span className={styles.itemText}>тактичні рюкзаки</span>
+                        <span className={styles.itemNumber}>15</span>
+                    </div>
+                    <div className={styles.item}>
+                        <span className={styles.itemText}>Бригада</span>
+                        <span className={styles.itemNumber}>128</span>
+                    </div>
+                    <div className={styles.quote}>
+                        <span className={styles.quoteText}>"</span>
+                        <p className={styles.quoteContent}>
+                            Ми, 128 бригада, стоїмо на передовій, де кожен день зустрічаємося з різноманітними викликами та труднощами. У зв'язку з постійною потребою у підтримці, ми звертаємося до вас з проханням про необхідність рюкзаків.
+                        </p>
+                    </div>
+                </div>
+                <div className={styles.rightSection1}>
+                    <img src="/main-page/extraHelp.svg" alt="Extra" className={styles.extraImage} />
+                    <div className={styles.learnMoreContainer}>
+                        <div className={styles.learnMore}>
+                            <p className={styles.learnMoreText}>Дізнатись більше</p>
+                            <img src="/main-page/arrow2.svg" alt="Arrow" className={styles.arrowIcon} />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
         </>
     );
 };
