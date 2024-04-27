1473cd584f39ab7562752a4c7c4e292171f4c930
vov4ik <volodymyr.vasylyshyn05@gmail.com>
2024-04-28 00:50:38 +0300

    implement set new password by email

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import styles from "./page.module.css";
 import Footer from '../components/Footer/Footer';
 import Section1 from '../components/Main/section1/section1';
 import HelpUkraine from '../components/Main/helpUkraine/helpUkraine';
-// import SupportAction from '../components/Main/SupportAction/support';
+import SupportAction from '../components/Main/SupportAction/support';
 import Header from '../components/Header/Header';
 import News  from '../components/Main/news/news';
 import ExtraHelp from '../components/Main/extraHelp/extraHelp';
@@ -14,7 +14,7 @@ export default function Home() {
     <Section1 />
     <HelpUkraine />
     <ExtraHelp />
-    {/*<SupportAction />*/}
+    <SupportAction />
     <News />
     </>
   );
